b5c54e01b45c3bd05758a50827b63b887577854f
MAC <mac@192.168.1.8>
2022-06-20 13:51:06 -0500

    Ajuste de estilos

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/login.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+let puerto = process.env.PORT || 3000;
+app.listen(puerto, () => {
+    console.log('Servidor corriendo en el puerto ' + puerto);
 });
